398440399e8e78d1d2a601254fcc26c69c4270f5
Ricardoaraujo00 <ricardo.araujo.t0114627@edu.atec.pt>
2020-07-02 17:45:15 +0100

    Criada funcção para consumir API para dicionário

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -21,7 +23,8 @@ namespace ConsoleApp1
     {
         static HttpClient client = new HttpClient();
         static string product = null;
-        
+
+        static string company = null;
 
         static void Main(string[] args)
         {
@@ -31,7 +34,22 @@ namespace ConsoleApp1
                 .Build();
 
             Console.WriteLine(configuration.GetSection("Company").Value);
-            
+
+            //Dictionary<string, string> kioskconfig = new Dictionary<string, string>();
+            //configuration.GetSection("kioske").GetChildren().ToList().ForEach(x => kioskconfig[x.Key] = x.Value);
+            //foreach (var item in kioskconfig)
+            //{
+            //    Console.WriteLine($"key: {item.Key}, Value: {item.Value}");
+            //}
+
+            //GetDataStringArray();
+            GetDataDictionary();
+
+        }
+
+        public static void GetDataDictionary()
+        {
+            Console.WriteLine("get dictionary");
             //Consuming the API
             //Specify web API base address
             client.BaseAddress = new Uri("http://localhost:53309/");
@@ -41,7 +59,43 @@ namespace ConsoleApp1
             var media = new MediaTypeWithQualityHeaderValue(val);
 
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(media)  ;
+            client.DefaultRequestHeaders.Accept.Add(media);
+
+            //Make the calls
+            try
+            {
+                var action = "api/kioskconfig";
+                var request = client.GetAsync(action);
+                var response =
+                    request.Result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("have result");
+                Dictionary<string,string> msgs = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+
+                foreach (var item in msgs)
+                {
+                    Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            Console.ReadKey();
+        }
+
+
+        public static void GetDataStringArray()
+        {
+            //Consuming the API
+            //Specify web API base address
+            client.BaseAddress = new Uri("http://localhost:53309/");
+
+            //Specify headers
+            var val = "application/json";
+            var media = new MediaTypeWithQualityHeaderValue(val);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(media);
 
             //Make the calls
             try
@@ -54,19 +108,32 @@ namespace ConsoleApp1
                 string[] msgs = JsonConvert.DeserializeObject<string[]>(response);
                 string msgsconcat = "";
                 int i = 0;
+                int compr = msgs.Length;
                 foreach (var item in msgs)
                 {
                     i++;
-                    Console.WriteLine(item);
-                    msgsconcat += item;
-                    if (i<3)
+                    if (i == 1)
                     {
-                        msgsconcat += "|";
+                        company = item;
+                        Console.WriteLine(company);
                     }
+                    else
+                    {
+                        Console.WriteLine(item);
+                        msgsconcat += item;
+                        if (i < compr)
+                        {
+                            msgsconcat += "|";
+                        }
+
+                    }
+
+
+
                 }
                 Console.WriteLine(msgsconcat);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
